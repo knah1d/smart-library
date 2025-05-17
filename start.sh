d0c2f17f6a301f5ed3fd5cfdb64d7fb4be02c5e3
knah1d <444nahid@gmail.com>
2025-05-17 21:47:39 +0600

    feat: Add start script to initialize services and client for the application

---
+++ start.sh
@@ -0,0 +1,25 @@
+sudo kill -9 $(sudo lsof -t -i :8081)
+sudo kill -9 $(sudo lsof -t -i :8082)
+sudo kill -9 $(sudo lsof -t -i :8083)
+# sudo kill -9 $(sudo lsof -t -i :5173)
+
+
+# Start User Service
+cd /home/nahid/Desktop/6th_SEM/DistSys/smart-library/user-service
+PORT=8081 npm run dev &
+
+
+# Start Book Service
+echo "Starting Book Service..."
+cd /home/nahid/Desktop/6th_SEM/DistSys/smart-library/book-service
+PORT=8082 npm run dev &
+
+# Start Loan Service
+echo "Starting Loan Service..."
+cd /home/nahid/Desktop/6th_SEM/DistSys/smart-library/loan-service
+PORT=8083 npm run dev &
+
+
+# Start Client
+cd /home/nahid/Desktop/6th_SEM/DistSys/smart-library/client
+PORT=5173 npm run dev 
